2018880cd6384441956128add491bef86459b7bc
Étienne Michon <etienne@scalingo.com>
2018-08-10 11:36:17 +0200

    Initial commit

---
+++ resources/assets/js/components/ScalingoFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <p>
+      <span class="one-liner"><a href="https://scalingo.com">Scalingo</a> is the best Platform as a Service on the market:</span>
+      no server to install, no dependencies to install, just push your code!
+    </p>
+  </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
